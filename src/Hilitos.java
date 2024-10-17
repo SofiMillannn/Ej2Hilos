5034a21590219cfa6bf40a256e6a2da0c87707e2
Sofia Millan <sofia.millanamigo@cieep.com>
2024-10-17 10:05:59 +0200

    added stuff

--- src/Hilitos.java
+++ src/Hilitos.java
@@ -1,5 +1,6 @@
 public class Hilitos extends Thread{
     static int nVocales;
+    static String frase = Main.getFrase();
     public Hilitos(String name) {
         super(name);
         start();
@@ -9,5 +10,17 @@ public class Hilitos extends Thread{
     @Override
     public void run() {
 
+        String[] fraseV = frase.split("");
+        if (getName().equals(getName())){
+            for (int i = 0; i < fraseV.length; i++) {
+              if (fraseV[i].equalsIgnoreCase(getName())){
+                  nVocales++;
+              }
+
+
+            }
+
+        }
+        System.out.println("Numero de vocales: " + nVocales);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static String frase;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Dime algo: ");
+        frase = br.readLine();
+        Thread hiloA = new Hilitos("A");
+        Thread hiloE = new Hilitos("E");
+        Thread hiloI = new Hilitos("I");
+        Thread hiloO = new Hilitos("O");
+        Thread hiloU = new Hilitos("U");
+
+        hiloA.join();
+        hiloE.join();
+        hiloI.join();
+        hiloO.join();
+        hiloU.join();
+
+    }
+
+    public static String getFrase() {
+        return frase;
+    }
+
+    public static void setFrase(String frase) {
+        Main.frase = frase;
     }
 }
