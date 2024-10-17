83e3515dba733ebda13ac2fafd438970559d31ae
Sofia Millan <sofia.millanamigo@cieep.com>
2024-10-17 09:32:36 +0200

    created classes

---
+++ src/Hilitos.java
@@ -0,0 +1,13 @@
+public class Hilitos extends Thread{
+    static int nVocales;
+    public Hilitos(String name) {
+        super(name);
+        start();
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
